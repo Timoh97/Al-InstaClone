a2ae1cda80c28d3b90da9eb569e2c68568ecacc3
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-04 11:14:25 +0300

    add attributes of image model

--- insta/models.py
+++ insta/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='images')
+    image = CloudinaryField('image')
+    image_name = models.CharField(max_length=50)
+    image_caption = models.TextField()
     def __str__(self):
         return self.
