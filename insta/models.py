bef917ba1bbd8a59a55e445fde802b938a2ee1fc
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-04 11:15:37 +0300

    add more attributes of image model

--- insta/models.py
+++ insta/models.py
@@ -7,5 +7,10 @@ class Image(models.Model):
     image = CloudinaryField('image')
     image_name = models.CharField(max_length=50)
     image_caption = models.TextField()
+    image_date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    like_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
+
     def __str__(self):
         return self.
