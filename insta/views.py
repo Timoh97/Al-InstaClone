34a8b80b5d94fe0ddb8857f51e7845b5475511bb
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-05 12:15:38 +0300

    create like_image method

--- insta/views.py
+++ insta/views.py
@@ -18,6 +18,31 @@ def profile(request):
     profile = Profile.objects.filter(user_id=current_user.id).first()
     return render(request, 'profile.html', {"images": images, "profile": profile})
     # return render(request, 'profile.html')
+    
+ef like_image(request, id):
+   likes = Likes.objects.filter(image_id=id).first()
+   # check if the user has already liked the image
+   if Likes.objects.filter(image_id=id, user_id=request.user.id).exists():
+       # unlike the image
+       likes.delete()
+       # reduce the number of likes by 1 for the image
+       image = Image.objects.get(id=id)
+       # check if the image like_count is equal to 0
+       if image.like_count == 0:
+           image.like_count = 0
+           image.save()
+       else:
+           image.like_count -= 1
+           image.save()
+       return redirect('/')
+   else:
+       likes = Likes(image_id=id, user_id=request.user.id)
+       likes.save()
+       # increase the number of likes by 1 for the image
+       image = Image.objects.get(id=id)
+       image.like_count = image.like_count + 1
+       image.save()
+       return redirect('/')
 
   
 
