cd87563242b3947ada5b8c84ecd6c11971178a81
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-05 08:55:44 +0300

    create LIcense and README.md

--- insta/urls.py
+++ insta/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    path('',views.index ,name = 'index'),
+    path('profile/', views.profile, name='profile'),
 ]
 
 if settings.DEBUG:
--- insta/views.py
+++ insta/views.py
@@ -3,5 +3,19 @@ from .models import *
 from django.http  import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to instaclone')
+
+
+def index(request):
+    # get all the images from the database and order them by the date they were created
+    images = Image.objects.all().order_by('-image_date')
+    return render(request, 'index.html', {'images': images})
+
+def profile(request):
+    current_user = request.user
+    # get images for the current logged in user
+    images = Image.objects.filter(user_id=current_user.id)
+    # get the profile of the current logged in user
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    return render(request, 'profile.html', {"images": images, "profile": profile})
+    # return render(request, 'profile.html')
+    
